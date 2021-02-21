9f7ab82c7725d7a32f0128a390e3c6c88ed6d591
alexdpow <powersad@sunypoly.edu>
2021-02-21 13:48:05 -0500

    Fixing Merge and some aesthetic adjustments
    -Added facing the right direction when going in different directions
    -Trying out lighting changes for some kind of retro look

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,41 +6,70 @@ using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour{
 
+    // Initial Variables
     private Rigidbody rb;
     private bool jumpKeyWasPressed;
-    private float horizontlInput ;
-    //SerializeField allows the varible to be set in Unity. I'm initializing these varibles here, because if they're initialized in start, then the values we set in unity is reset.
+    private float horizontlInput;
+    [SerializeField] private bool lockZAxis = false;
+    // SerializeField allows the varible to be set in Unity. I'm initializing these varibles here, because if they're initialized in start, then the values we set in unity is reset.
     [SerializeField] private int jumpCount = 1;
-    [SerializeField] private float movespeed = 2f; 
+    [SerializeField] private float movespeed = 5f; 
     [SerializeField] private Transform groundCheckTransform = null;
     [SerializeField] private LayerMask playerMask;
-
+    
+    //Jump vars
+    public float jumpTimer;
+    [SerializeField] public float jumpDeltaTime = 0.3f;      //how long to wait between jumps
+    [SerializeField] public float fallMultiplier = 2f;
+    
+    public Transform modelchild;
+    
     // Start is called before the first frame update
     void Start(){
-
+        modelchild = this.gameObject.transform.GetChild(0);
         rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update(){
-
-        //Jump and movement update, this makes it so the movement is set to occur at the next physics update, rather than this frame update.
-        if (Input.GetKeyDown(KeyCode.Space)){
-
-            jumpKeyWasPressed=true; 
+        float XAxisInput = Input.GetAxis("Horizontal");
+        float ZAxisInput = Input.GetAxis("Vertical");
+        // Jump and movement update, this makes it so the movement is set to occur at the next physics update, rather than this frame update.
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > jumpTimer && jumpCount > 0){
+            rb.AddForce(Vector3.up * 450);
+            jumpTimer = Time.time + jumpDeltaTime;
+         }
+        
+        // speed up the fall of the jump to make it not feel as "floaty" (work in progress)
+        if(rb.velocity.y < 0){
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        horizontlInput = Input.GetAxis("Horizontal");
-
-    
-        //RESET
+        
+        // Movement
+        if (!lockZAxis) {
+            transform.Translate(movespeed * XAxisInput * Time.deltaTime, 0f, movespeed * ZAxisInput * Time.deltaTime);
+        } 
+        else {
+            transform.Translate(movespeed * XAxisInput  * Time.deltaTime, 0f, 0);
+        }
+        
+        // Change rotation
+        if (XAxisInput > 0) {
+            modelchild.localRotation = Quaternion.Euler(0,110,0);
+        }
+        else if (XAxisInput < 0) {
+            modelchild.localRotation = Quaternion.Euler(0,-110,0);
+        } 
+            
+        // RESET
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene("Level2");
         }
     }
 
-    //Called once every physics update
+    // Called once every physics update
     private void FixedUpdate()
     {
         //rb.interpolation = RigidbodyInterpolation.Interpolate;
@@ -68,28 +97,18 @@ public class player : MonoBehaviour{
         }
 
     }
-
-    //CLICK TO KILL PLAYER
-    private void OnMouseDown(){
-
-        Destroy(gameObject);
-    }
-
+   
     //COLLISION
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Enemy")
-        {
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Enemy"){
             Destroy(gameObject);
             //Destroy(collision.gameObject); 
             
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.layer == 9) //if the player hits a coin, give them a double jump
-        {
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.layer == 9){ //if the player hits a coin, give them a double jump
             Destroy(other.gameObject);
             jumpCount = 2;
         }
