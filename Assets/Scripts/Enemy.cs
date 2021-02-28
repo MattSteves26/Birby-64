05910232d21a77dec953ac23ed04d0ffd4a14482
alexdpow <powersad@sunypoly.edu>
2021-02-28 16:32:44 -0500

    Merge Fixing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,27 @@ public class Enemy : MonoBehaviour
 {
     
     private Rigidbody rb;
-    private bool isGrounded;
     public float health = 3;
     public float maxHealth = 3;
     [SerializeField] private Slider healthBar;
+    
+    private bool movingRight = true;
+
+    public Transform player;
+    public float range = 50.0f;
+  
+    private bool onRange = false;
+
+    public float distance;
+
+    private Vector3 pos1 = new Vector3(-4, 0, 0);
+    private Vector3 pos2 = new Vector3(4, 0, 0);
+    public float speed = 1.0f;
+
+    public Transform groundDetect;
+
+    private bool isGrounded;
+
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +38,10 @@ public class Enemy : MonoBehaviour
 
     void Update(){
         healthBar.value = health/maxHealth;
+        
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+
+        onRange = Vector3.Distance(transform.position, player.position) < range;
     }
 
    
@@ -29,7 +50,7 @@ public class Enemy : MonoBehaviour
     {
         if(isGrounded)
         {
-            rb.AddForce(Vector3.up * 50f);
+            rb.AddForce(Vector3.up * 25);
         }
     }
 
