b667e69c83a0267dde7d8ac53786b801f123272d
Thecontrollers <mandudegaming@gmail.com>
2021-03-22 04:44:31 -0400

    Added a horrible(but functional) options menu
    and a crused song pls don't hurt me

---
+++ Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+    
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public TMPro.TMP_Dropdown resolutionsDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+
+        int currentResolutionsIndex = 0;
+        int i = 0;
+        List<string> options = new List<string>();
+
+        foreach (Resolution x in resolutions)
+        {
+            string option = x.width + "x" + x.height;
+            options.Add(option);
+
+            if(x.width == Screen.currentResolution.width && x.height == Screen.currentResolution.height)
+            {
+                currentResolutionsIndex = i;
+            }
+            i++;
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionsIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen);
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
