04c8cfb23b4b21b84e89634b379cdfaef242300d
Thecontrollers <mandudegaming@gmail.com>
2021-03-28 02:51:45 -0400

    Changed some menu stuff
    I also fixed the health bar

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+    public Canvas mainmenu;
+    public Canvas LevelSection;
+    public Canvas Options;
 
+    private static int levelmenu_switch = 1;
+    private static int optionmenu_switch = 1;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,5 +21,37 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("quit");
         Application.Quit();
+    }
+
+    public void LevelSelection()
+    {
+        if (levelmenu_switch == 1)
+        {
+            mainmenu.enabled = false;
+            LevelSection.enabled = true;
+            levelmenu_switch = 0;
+        }
+        else
+        {
+            mainmenu.enabled = true;
+            LevelSection.enabled = false;
+            levelmenu_switch = 1;
+        }
+    }
+
+    public void OptionsMenu()
+    {
+        if(optionmenu_switch == 1)
+        {
+            mainmenu.enabled = false;
+            Options.enabled = true;
+            optionmenu_switch = 0;
+        }
+        else
+        {
+            mainmenu.enabled = true;
+            Options.enabled = false;
+            optionmenu_switch = 1;
+        }
     }
 }
