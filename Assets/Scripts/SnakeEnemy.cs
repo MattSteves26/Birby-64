9da46cbe36326ab3fff02ac4f5750fe91a13ec2c
MSteves26 <78507705+MSteves26@users.noreply.github.com>
2021-04-11 20:50:36 -0400

    Add files via upload

---
+++ Assets/Scripts/SnakeEnemy.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SnakeEnemy : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float health = 3;
+    public float maxHealth = 3;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private int damage = 1;
+    public float moveTimer;
+    [SerializeField] public float moveDeltaTime = 1.5f;
+
+    public Transform player;
+    public float range = 7.0f;
+
+    private bool chasePlayer = false;
+    private bool goingRight;
+
+    public float distance;
+    public float speed = 1.0f;
+
+    public bool SnakeJump;
+    public float jumpDistance;
+    public float jumpRange;
+    int jump = 0;
+    public Vector3 jumping;
+    public float jumpForce = 2.0f;
+    public float jumpForceAttack = 6.0f;
+    int jumpAttack = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        healthBar.value = health / maxHealth;
+
+        jumping = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void Update()
+    {
+        healthBar.value = health / maxHealth;
+        distance = Vector2.Distance(transform.position, player.position);
+        jumpDistance = Vector2.Distance(transform.position, player.position);
+        Debug.Log(distance);
+        Debug.Log(jumpDistance);
+
+        if (distance < range)
+        {
+            chasePlayer = true;
+        }
+        else
+        {
+            chasePlayer = false;
+        }
+
+        if (jumpDistance < jumpRange)
+        {
+            SnakeJump = true;
+        }
+        else
+        {
+            SnakeJump = false;
+        }
+
+
+    }
+
+
+    // FixedUpdate is called once per physics update
+    void FixedUpdate()
+    {
+
+        if (SnakeJump == true && jump == 0)
+        {
+            rb.AddForce(jumping * jumpForce, ForceMode.Impulse);
+
+            jump = 1;
+        }
+
+        if (chasePlayer && Time.time > moveTimer && jumpAttack == 0)
+        {
+            if (transform.position.x < player.position.x)
+            {
+                rb.AddForce(jumping * jumpForceAttack, ForceMode.Impulse);
+                rb.velocity = new Vector3(speed, 0, 0);
+                goingRight = true;
+            }
+            else
+            {
+                rb.AddForce(jumping * jumpForceAttack, ForceMode.Impulse);
+                rb.velocity = new Vector3(-speed, 0, 0);
+                goingRight = false;
+            }
+
+            jumpAttack = 1;
+
+        }
+
+        if (chasePlayer && Time.time > moveTimer && jumpAttack == 1)
+        {
+            if (transform.position.x < player.position.x)
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                goingRight = true;
+            }
+            else
+            {
+                rb.velocity = new Vector3(-speed, 0, 0);
+                goingRight = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.GetComponent<playerHealth>().TakeDamage(damage);
+            if (goingRight == true)
+            {
+                rb.AddForce(new Vector3(-4, 1, 0), ForceMode.Impulse);
+                moveTimer = Time.time + moveDeltaTime;
+            }
+            else
+            {
+                rb.AddForce(new Vector3(4, 1, 0), ForceMode.Impulse);
+                moveTimer = Time.time + moveDeltaTime;
+            }
+
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
