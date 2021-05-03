aa4bf7bb54e905f7d34bdad5f4d232294c623101
Devon McClendon <mdevon35@gmail.com>
2021-05-03 13:58:23 -0400

    credits goes back to main menu

---
+++ Assets/loadScene.cs
@@ -0,0 +1,15 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.SceneManagement;
+
+
+ public class loadScene : MonoBehaviour
+ {
+     public void LoadNewScene()
+     {
+        //go back to menu
+        SceneManager.LoadScene(0);  
+     }
+ }
+ 
