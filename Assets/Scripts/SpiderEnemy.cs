6fe41111d793935dc8a59ced2eb35e494c099ed1
MSteves26 <78507705+MSteves26@users.noreply.github.com>
2021-05-07 10:43:46 -0400

    Update SpiderEnemy.cs

--- Assets/Scripts/SpiderEnemy.cs
+++ Assets/Scripts/SpiderEnemy.cs
@@ -4,6 +4,9 @@ Speed Changes how fast the spider moves
 Bounce Determines how high the spider bounces
 Travel Distance Determines how far the spider travels
 For example, if u want the spider to move 6 on the x axis, set it to 6 and it will travel from where it was placed then to the right 6 over the x axis
+
+This is for spider enemy
+*Works in final version, but the not placed in any levels
 */
 using System.Collections;
 using System.Collections.Generic;
@@ -20,9 +23,12 @@ public class SpiderEnemy : MonoBehaviour
 
     public float speed = 1.0f;
 
+
+//Change this in unity editor to determine the distance you travel
     public float min;
     public float max;
 
+//Change this in unity editor to determine how high you jump
     public float bounce;
 
     public float travelDistance;
@@ -37,17 +43,22 @@ public class SpiderEnemy : MonoBehaviour
 
     void Update()
     {
+    
+    //This performs the movement
         transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
     }
 
     void OnCollisionEnter(Collision collide)
     {
+    
+    //This bounces the player
         if (collide.gameObject.tag == "Ground")
         {
             rb.velocity = new Vector3(0, 0);
             rb.AddForce(new Vector3(0, bounce), ForceMode.Impulse);
         }
 
+    //This damages the player
         if (collide.gameObject.tag == "Player")
         {
             collide.collider.GetComponent<playerHealth>().TakeDamage(damage);
